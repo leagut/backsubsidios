3db20f938f08e7c00f66be1990998e50c3d8ff48
leandro <leaguts21@gmail.com>
2022-11-29 18:46:26 -0500

    primer commit

---
+++ subsidios/src/main/java/com/leandro/subsidios/SubsidiosDelEstadoApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.subsidios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsidiosDelEstadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsidiosDelEstadoApplication.class, args);
+	}
+
+}
